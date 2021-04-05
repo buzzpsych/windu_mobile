e6625b442d1eabfce31d355830d9e9bc4e42edea
evan fishman <fishyvisions@evans-MacBook-Pro.local>
2021-04-04 22:14:47 -0400

    init

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image
+        source={{
+          uri: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        }}
+        style={{
+          marginTop: 30,
+          width: 40,
+          height: 40,
+          resizeMode: "contain",
+        }}
+      />
     </View>
   );
 }
@@ -14,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
